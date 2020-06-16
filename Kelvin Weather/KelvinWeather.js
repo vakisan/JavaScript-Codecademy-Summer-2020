e0355cb03141db1d1c9f643f11938b0a55c06507
vakisan <vakisanprogrammer@gmail.com>
2020-06-16 18:31:26 +0100

    added extension task | celcius to newton convertion added

--- Kelvin Weather/KelvinWeather.js
+++ Kelvin Weather/KelvinWeather.js
@@ -15,3 +15,7 @@ let celcius = kelvin - 273;
 let fahrenheit = Math.floor(celcius * (9 / 5) + 32);
 
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+let newton = Math.floor(celcius * (33 / 100));
+
+console.log(`The temperature is ${newton} degrees Newton.`);
